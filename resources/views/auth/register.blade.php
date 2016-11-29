11b87203f0f2d3f442cc196861809c14d80f2c0a
p_pandya <chintu.pandya98@gmail.com>
2016-11-29 16:12:30 -0500

    login and register , awesome font icon

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,7 +63,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    <i class="fa fa-user-circle"></i> <span>Register</span>
                                 </button>
                             </div>
                         </div>
