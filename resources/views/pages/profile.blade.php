0a91dce44f40b317a0518654df7e59b581b2c05a
p_pandya <chintu.pandya98@gmail.com>
2016-12-14 17:55:59 -0500

    profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,19 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="jumbotron">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Profile</div>
-                    <div class="panel-body">
-                        <h1>Hello, {!! $user->name !!} </h1>
-                        You are logged in {!! $user->email !!}!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <link href="/css/custome/profile.css" rel="stylesheet">
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
@@ -33,6 +21,116 @@
                                  </form>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+
+                    <div class="panel-body">
+                        <div class="col-md-12">
+                            <h3>Basic Info</h3><span></span>
+
+                        </div>
+                        <form class="form-horizontal" role="form">
+                            <div class="form-group">
+                                <label for="email" class="col-md-5 control-label">Email</label>
+
+                                <div class="col-md-6">
+                                    <span >{{$user->email}}</span>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cardNumber" class="col-md-5 control-label">Name</label>
+
+                                <div class="col-md-6">
+                                    <span>{{$user->name}}</span>
+
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cvc" class="col-md-5 control-label">Status</label>
+                                <div class="col-md-6">
+                                    <span id="user-status">{{ucfirst($user->user_status)}} User</span>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="col-md-12">
+                            <h3>Gift Sent</h3><span></span>
+                        </div>
+
+                        <form class="form-horizontal" role="form">
+                            <div class="form-group">
+                                <label for="email" class="col-md-5 control-label">Last Gift Amount</label>
+
+                                <div class="col-md-6" style="margin-top: 10px;" >
+                                    <span class="alert alert-danger">0 $</span>
+                                </div>
+                            </div>
+
+                            <div class="form-group" >
+                                <label for="cardNumber" class="col-md-5 control-label" style="margin-top: 5px;">Total Gift Amount</label>
+
+                                <div class="col-md-6" style="margin-top: 20px;">
+                                    <span class="alert alert-danger">0 $</span>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="col-md-12">
+                            <h3>Earned Credit</h3>
+                        </div>
+
+                        <form class="form-horizontal" role="form">
+                            <div class="form-group">
+                                <label for="email" class="col-md-5 control-label">Earned Points</label>
+
+                                <div class="col-md-3" style="margin-top: 10px;" >
+                                    <span class="alert alert-danger">0 $</span>
+                                </div>
+                            </div>
+
+                            <div class="form-group" >
+                                <label for="cardNumber" class="col-md-5 control-label" style="margin-top: 5px;">Earned Money</label>
+
+                                <div class="col-md-3" style="margin-top: 20px;">
+                                    <span class="alert alert-danger">0 $</span>
+                                </div>
+                                <div class="col-md-3" style="margin-top: 20px;">
+                                    <a href="/learnHow" class="btn btn-warning pull-right">Learn More</a>
+                                </div>
+                            </div>
+
+                            <div class="form-group" >
+
+
+                            </div>
+                        </form>
+
                     </div>
                 </div>
             </div>
